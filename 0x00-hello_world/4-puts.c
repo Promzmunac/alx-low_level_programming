b9449ae3f453c59ad956b0998b0af056a6c14e85
Promzmunac <nnapromise7@gmail.com>
2023-08-10 14:24:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
+	puts("\n"Programming is like building a multilingual puzzle,");
 	return (0);
 }
