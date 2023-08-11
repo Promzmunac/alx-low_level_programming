86cbc92dd885463918d7627f03421c653fb9fe57
Promzmunac <nnapromise7@gmail.com>
2023-08-11 16:36:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
-* main - A C program that prints a line to the standard error
-*
-*return 1 (success)
-*/
+ * main - A C programme that prints a line to the standard error
+ * Return 1 (success)
+ *
+ */
+
 int main(void)
+
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (1);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+
+	return (1);
 }	
